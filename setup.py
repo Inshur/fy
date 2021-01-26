c8f8d8026a589b0c4baa973ccd89d6c02bbe50d8
Rob Wilson <roobert@gmail.com>
2021-01-26 19:39:57 +0000

    Bump GCP SDK version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ readme = ""
 setup(
     long_description=readme,
     name="fy",
-    version="1.0.13",
+    version="1.0.14",
     python_requires="==3.*,>=3.7.0",
     author="Rob Wilson",
     author_email="roobert@gmail.com",
